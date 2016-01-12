2dcc1cdc950277c957cba2bc963618f76b2c8090
Wayne Warren <waynr+launchpad@sdf.org>
2016-01-11 23:34:28 -0800

    Step 1: Create basic job that does nothing.

---
+++ examples/jenkins-manager/pipeline.py
@@ -0,0 +1,16 @@
+import jenkins_manager.types.job as job
+
+
+class BaseJob(job.Job):
+
+    def __init__(self, *args, **kwargs):
+        super(BaseJob, self).__init__(*args, **kwargs)
+
+
+def get_jobs():
+
+    job1 = BaseJob({
+        "name": "jenkins-manager",
+    })
+
+    return [job1]
