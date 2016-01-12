14104be2ba379001bba806909435f64ec2c156fb
Wayne Warren <waynr+launchpad@sdf.org>
2016-01-11 23:45:56 -0800

    Step 3: Add execute-shell builder.

--- examples/jenkins-manager/pipeline.py
+++ examples/jenkins-manager/pipeline.py
@@ -11,7 +11,29 @@ def get_jobs():
 
     job1 = BaseJob({
         "name": "jenkins-manager",
-        "display-name": "jenkins-manager Noop Job",
+        "display-name": "jenkins-manager Test Job",
     })
+    job1.builders = [
+        {"shell": """#!/usr/bin/env bash
+set -e
+set -x
+
+echo "Setting up and activating virtualenv"
+virtualenv .virtualenv && source .virtualenv/bin/activate
+
+echo "Installing Python testing requirements"
+pip install -r requirements-test.txt
+
+echo "Installing Jenkins Job Builder 2.x"
+git clone -b jjb-2.0.0-api https://github.com/waynr/jenkins-job-builder
+pip install -e ./jenkins-job-builder
+
+echo "Installing Jenkins Manager"
+pip install -e ./
+
+echo "Actually run the tests..."
+python setup.py testr --slowest
+        """},
+    ]
 
     return [job1]
